33eb2b9f621b5e999ce728d7aa4c59945c4fa7d7
Ray <rayhixson@users.noreply.github.com>
2025-03-03 11:32:23 -0800

    more mode-line cleanup to my likings

--- lisp/init-experimental.el
+++ lisp/init-experimental.el
@@ -1,11 +1,9 @@
 ;; These are packages I'm testing out.
 ;; When I decide to keep them I will move them to a more permanent package
 
-;; https://github.com/bling/fzf.el
-
-(use-package casual-dired
-  :straight t
-  :ensure t
-  :bind (:map dired-mode-map ("C-o" . 'casual-dired-tmenu)))
+;; (use-package casual-dired
+;;   :straight t
+;;   :ensure t
+;;   :bind (:map dired-mode-map ("C-o" . 'casual-dired-tmenu)))
 
 (provide 'init-experimental)
--- lisp/init-modeline.el
+++ lisp/init-modeline.el
@@ -4,6 +4,8 @@
 (defvar absolute-filename-subs
   '(("/Users/rhixson/" . "[~]")
 		("/Users/rhixson/dev" . "[dev]")
+    ("/Users/ray/" . "[~]")
+		("/Users/ray/dev" . "[dev]")
     ("/$" . "")
     ("/Users/rhixson/dev/terraform/squads/" . "[squads]/")
     ("/Users/rhixson/dev/terraform/infra/" . "[infra]/")
@@ -12,51 +14,15 @@
 (defun modeline-dir-abbrev ()
   (str-replace-all default-directory absolute-filename-subs))
 
+;; (use-package spacious-padding
+;;   :straight t
+;;   :hook (after-init . spacious-padding-mode)
+;;   :custom
+;;   ;; make the modeline look minimal
+;;   (spacious-padding-subtle-mode-line '( :mode-line-active default
+;;                                         :mode-line-inactive vertical-border)))
 
-(use-package spacious-padding
-  :straight t
-  :hook (after-init . spacious-padding-mode)
-  :custom
-  ;; make the modeline look minimal
-  (spacious-padding-subtle-mode-line '( :mode-line-active default
-                                        :mode-line-inactive vertical-border)))
-
-(defface appt-notification
-  '((t :foreground "#fff" :background "#333"))
-  "Face for appointment notifications.")
-
-(defvar ml-text-scale-factor 1.0
-  "Scale of mode-line font size to default font size, as a float.
-This is needed to make sure that text is properly aligned.")
-
-(defun ml-fill-to-center (reserve face)
-  "Return empty space to the center, leaving RESERVE space on the right."
-  (when ml-text-scale-factor
-    (setq reserve (* ml-text-scale-factor reserve)))
-  (propertize " "
-              'display `((space :align-to (- (+ center (.5 . right-margin))
-                                             ,reserve
-                                             (.5 . left-margin))))
-              'face face))
-
-(defun ml-fill-to-right (reserve face)
-  "Return empty space, leaving RESERVE space on the right."
-  (when ml-text-scale-factor
-    (setq reserve (* ml-text-scale-factor reserve)))
-  (when (and window-system (eq 'right (get-scroll-bar-mode)))
-    (setq reserve (- reserve 2))) ; Powerline uses 3 here, but my scrollbars are narrower.
-  (propertize " "
-              'display `((space :align-to (- (+ right right-fringe right-margin)
-                                             ,reserve)))
-              'face face))
-
-(defun ml-render-2-part (left right &optional fill-face)
-  (concat left
-          (ml-fill-to-right (string-width (format-mode-line right)) fill-face)
-          right))
-
-
-;; determin the focused window
+;; determine the focused window
 (defvar cogent-line-selected-window (frame-selected-window))
 (defun cogent-line-set-selected-window (&rest _args)
   (when (not (minibuffer-window-active-p (frame-selected-window)))
@@ -71,90 +37,12 @@ This is needed to make sure that text is properly aligned.")
 (add-hook 'window-configuration-change-hook #'cogent-line-set-selected-window)
 (add-hook 'window-selection-change-functions #'cogent-line-set-selected-window)
 
-(defun tab-bar-tab-name-format-comfortable (tab i)
-  "Add spacing to tab bar mode"
-  (propertize (concat " " (truncate-string-to-width
-                           (tab-bar-tab-name-format-default tab i)
-                           tab-bar-tab-name-truncated-max
-                           nil nil t)
-                      " ")
-              'face (funcall tab-bar-tab-face-function tab)))
-(setq my/mode-line-left-side '(" "
-                               ;; indicate if the buffer has been modified
-                               (:eval (propertize (if (and (not buffer-read-only) (buffer-modified-p))
-                                                      "● " "  " )
-                                                  'face 'error))
-
-                               ;; Buffer name (no longer than 1/3 of the screen)
-                               ;; mode-line-remote
-                               mode-line-buffer-identification
-                               (:eval (format " [%s]" (modeline-dir-abbrev)))
-                               mode-line-process
-                               ;; (:eval (truncate-string-to-width
-                               ;;         (buffer-name)
-                               ;;         50
-                               ;;         nil nil t))
-                               ;; " "
-                               (:eval (when (mode-line-window-selected-p)" %l:%c"))
-                               ;; (:eval (when (mode-line-window-selected-p)
-                               ;;          (propertize
-                               ;;           (concat " %p%"
-                               ;;                   "  "
-                               ;;                   "「 %m 」")
-                               ;;           'face (if (cogent-line-selected-window-active-p)
-                               ;;                     'shadow
-                               ;;                   'mode-line-inactive))))
-                               ))
-
-(setq my/mode-line-padding
-      '(:eval
-        (propertize " " 'display
-	                  `((space :align-to
-			                       (- (+ right right-fringe right-margin)
-			                          ,(string-width
-                                  (format-mode-line my/mode-line-right-side))))))))
-
-(setq my/mode-line-right-side '(:eval (when (mode-line-window-selected-p)
-                                        (list
-                                         '(vc-mode vc-mode)
-                                         mode-line-misc-info
-                                         )
-                                        )))
-
-;; (defun vc-branch ()
-;;   (let ((backend (vc-backend buffer-file-name)))
-;;     (substring vc-mode (+ (if (eq backend 'hg) 2 3) 2))))
-;; (vc-branch)
-
-;; (set-face-attribute 'mode-line-inactive nil :foreground "#555" :background "#222"
-;;                     :box '(:line-width 2 :color "#222"))
-;; (use-package time
-;;   :defer 10
-;;   :config
-;;   (defface my/display-time
-;;     '((((type x w32 mac))
-;;        ;; #060525 is the background colour of my default face.
-;;        (:foreground "#060525" :inherit bold))
-;;       (((type tty))
-;;        (:foreground "blue")))
-;;     "Face used to display the time in the mode line.")
-;;   (setq display-time-string-forms
-;;         '((propertize (concat " " 12-hours ":" minutes " " am-pm " ")
-;;                       'face 'my/display-time)))
-;;   (set-face-attribute 'my/display-time nil :foreground "#fff" :background "#333"
-;;                       ;; :box '(:line-width 1 :color "#323")
-;;                       )
-;;   (display-time-mode t))
-
-(set-face-attribute 'appt-notification nil :foreground "#fff" :background "#333")
-
-(set-face-attribute  'org-mode-line-clock nil :foreground "#fff" :background "#333"
-                     ;; :box '(:line-width 1 :color "#323")
-                     :inherit 'bold)
-
-
-
 ;;---------------------
+;;(set-face-attribute 'mode-line-inactive nil :foreground "#555" :background "#222"
+;;                    :box '(:line-width 2 :color "#222"))
+
+(setq rays/mode-line-sep (propertize " | " 'face '(:foreground "hot pink")))
+
 (setq rays/mode-line-left-side '(""
                                  ;; indicate if the buffer has been modified
                                  (:eval (propertize (if (and (not buffer-read-only) (buffer-modified-p))
@@ -171,16 +59,14 @@ This is needed to make sure that text is properly aligned.")
 			                          ,(string-width
                                   (format-mode-line rays/mode-line-right-side))))))))
 
-(setq rays/mode-line-sep (propertize " | " 'face '(:foreground "hot pink")))
-
 (setq rays/mode-line-right-side '(:eval (when (mode-line-window-selected-p)
                                           (list
                                            "   ["
                                            '(:eval (modeline-dir-abbrev))
                                            "]"
                                            rays/mode-line-sep
-                                           '(vc-mode vc-mode)
-                                           rays/mode-line-sep
+;;                                           '(vc-mode vc-mode)
+;;                                           rays/mode-line-sep
                                            "%[("
                                            'mode-name
                                            "%n"
@@ -193,12 +79,13 @@ This is needed to make sure that text is properly aligned.")
                                            '(-3 . "%P")
                                            "  "
                                            'mode-line-misc-info
+                                           "-%-"
                                            ))))
 (setq-default mode-line-format
               `(
                 ,rays/mode-line-left-side
                 ;; I still have yet to come up with a better option
-                ,rays/mode-line-padding
+                ;; ,rays/mode-line-padding
                 ,rays/mode-line-right-side))
 
 
